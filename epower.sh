fbe985c0ecff3eacdd7e76bf8334f36a6352aeea
elmerpabel <48300603+elmerpabel@users.noreply.github.com>
2019-12-04 23:22:24 -0500

    Update epower.sh

--- epower.sh
+++ epower.sh
@@ -1,13 +1,7 @@
-#!/usr/bin/env bash
-################################################################################
-# epower.sh - envia comando a google
-################################################################################
-# direccion de google
-web="https://script.google.com/macros/s/AKfycbzocdEL91in-LlXNETRzNPePkEnIcfQsTV3Lz-sYD9--GkxhRg/exec"
-now_milisegundos=$(date +%s000)
-power=$(cat /proc/power/active_pwr1)
-now_utc=$(date -u '+%Y-%m-%d'T'%H:%M:%S'';')
-parameter="?player=Amigo&msg="$now_utc$power
-link=$web$parameter
-wget -c --no-check-certificate $link 
-
+web="https://script.google.com/macros/s/AKfycbzocdEL91in-LlXNETRzNPePkEnIcfQsTV3Lz-sYD9--GkxhRg/exec";
+now_milisegundos=$(date +%s000);
+power=$(cat /proc/power/active_pwr1);
+now_utc=$(date -u '+%Y-%m-%d'T'%H:%M:%S'';');
+parameter="?player=Amigo&msg="$now_utc$power;
+link=$web$parameter;
+wget -c --no-check-certificate $link;
